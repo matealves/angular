94f8cee7ef9dea4e6380c81145f7d0ad484e7049
Mateus Alves <contatomateusalves@hotmail.com>
2024-04-25 19:59:59 -0300

    ⚙️manipulando dados

---
+++ classes/02_interpolacao-de-dados/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
