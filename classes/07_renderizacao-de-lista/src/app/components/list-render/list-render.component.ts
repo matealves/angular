3888f3523034524bc88dfc9195f5106d14c6c316
Mateus Alves <contatomateusalves@hotmail.com>
2024-04-30 19:01:35 -0300

    ⚙️renderizando listas

--- classes/07_renderizacao-de-lista/src/app/components/list-render/list-render.component.ts
+++ classes/07_renderizacao-de-lista/src/app/components/list-render/list-render.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
-  styleUrl: './list-render.component.css'
+  styleUrl: './list-render.component.css',
 })
 export class ListRenderComponent {
-
+  animals = [
+    { name: 'Turca', type: 'Dog' },
+    { name: 'Tom', type: 'Cat' },
+    { name: 'Frida', type: 'Dog' },
+    { name: 'Bob', type: 'Horse' },
+  ];
 }
