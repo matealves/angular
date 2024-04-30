257a88e62cc1365a1078663fdefc4c8c98a6ba28
Mateus Alves <contatomateusalves@hotmail.com>
2024-04-30 19:19:53 -0300

    ⚙️Utilizando interfaces TS

--- classes/07_renderizacao-de-lista/src/app/components/list-render/list-render.component.ts
+++ classes/07_renderizacao-de-lista/src/app/components/list-render/list-render.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 
+import { Animal } from '../../Animal';
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrl: './list-render.component.css',
 })
 export class ListRenderComponent {
-  animals = [
-    { name: 'Turca', type: 'Dog' },
-    { name: 'Tom', type: 'Cat' },
-    { name: 'Frida', type: 'Dog' },
-    { name: 'Bob', type: 'Horse' },
+  animals: Animal[] = [
+    { name: 'Turca', type: 'Dog', age: 5 },
+    { name: 'Tom', type: 'Cat', age: 2 },
+    { name: 'Frida', type: 'Dog', age: 3 },
+    { name: 'Bob', type: 'Horse', age: 8 },
   ];
+
+  animalDetails = '';
+
+  showAge(animal: Animal) {
+    this.animalDetails = `O pet ${animal.name}, tem ${animal.age} anos de idade.`;
+  }
 }
