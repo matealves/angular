c511c17165b620638c2488017fc5d8449f7a190e
Mateus Alves <contatomateusalves@hotmail.com>
2024-05-09 14:55:06 -0300

    ⚙️services

--- classes/10_services/src/app/components/list-render/list-render.component.ts
+++ classes/10_services/src/app/components/list-render/list-render.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { ListService } from '../../services/list.service';
 
 import { Animal } from '../../Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrl: './list-render.component.css',
 })
 export class ListRenderComponent {
+  constructor(private listService: ListService) {}
+
   animals: Animal[] = [
     { name: 'Turca', type: 'Dog', age: 5 },
     { name: 'Tom', type: 'Cat', age: 2 },
@@ -18,5 +22,10 @@ export class ListRenderComponent {
 
   showAge(animal: Animal) {
     this.animalDetails = `O pet ${animal.name}, tem ${animal.age} anos de idade.`;
+  }
+
+  removeAnimal(animal: Animal) {
+    console.log('Removendo animal...');
+    this.animals = this.listService.remove(this.animals, animal);
   }
 }
