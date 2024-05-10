2e967015292bfbcdcffbd3a6a74cb0d90fcf6d79
Mateus Alves <contatomateusalves@hotmail.com>
2024-05-10 15:13:31 -0300

    ⚙️excluindo dados pelo Service

---
+++ classes/14_excluindo-dados-pelo-Service/src/app/components/list-render/list-render.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { ListService } from '../../services/list.service';
+
+import { Animal } from '../../Animal';
+
+@Component({
+  selector: 'app-list-render',
+  templateUrl: './list-render.component.html',
+  styleUrl: './list-render.component.css',
+})
+export class ListRenderComponent {
+  constructor(private listService: ListService) {
+    this.getAnimals();
+  }
+
+  animals: Animal[] = [];
+
+  animalDetails = '';
+
+  showAge(animal: Animal): void {
+    this.animalDetails = `O pet ${animal.name}, tem ${animal.age} anos de idade.`;
+  }
+
+  getAnimals() {
+    this.listService.getAll().subscribe((animals) => (this.animals = animals));
+  }
+
+  removeAnimal(animal: Animal): void {
+    console.log('Removendo animal...');
+    this.animals = this.listService.remove(this.animals, animal);
+  }
+}
