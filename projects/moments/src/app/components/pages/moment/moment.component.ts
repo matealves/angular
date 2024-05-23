7eb545810b6d01da6b5397a7b9e8185b8e43a5d7
Mateus Alves <contatomateusalves@hotmail.com>
2024-05-23 18:39:51 -0300

    ✔️finalizando projeto

--- projects/moments/src/app/components/pages/moment/moment.component.ts
+++ projects/moments/src/app/components/pages/moment/moment.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormGroupDirective,
+} from '@angular/forms';
 
 import { MomentService } from '../../../services/moment.service';
 import { MessagesService } from '../../../services/messages.service';
+import { CommentService } from '../../../services/comment.service';
 
 import { Moment } from '../../../interfaces/Moment';
+import { Comment } from '../../../interfaces/Comment';
+
 import { environment } from '../../../../environments/environment.development';
 
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
@@ -21,12 +30,15 @@ export class MomentComponent {
   faTimes = faTimes;
   faEdit = faEdit;
 
+  commentForm!: FormGroup;
+
   id = 1;
 
   constructor(
     private momentService: MomentService,
     private route: ActivatedRoute,
     private messagesService: MessagesService,
+    private commentService: CommentService,
     private router: Router
   ) {}
 
@@ -39,6 +51,18 @@ export class MomentComponent {
     if (typeof document !== 'undefined') {
       document.documentElement.scrollIntoView();
     }
+
+    this.commentForm = new FormGroup({
+      text: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.required),
+    });
+  }
+
+  get text() {
+    return this.commentForm.get('text')!;
+  }
+  get username() {
+    return this.commentForm.get('username')!;
   }
 
   async removeHandler(id: number) {
@@ -46,5 +70,21 @@ export class MomentComponent {
 
     this.messagesService.add('Momento excluído com sucesso!');
     setTimeout(() => this.router.navigate(['/']), 2500);
+  }
+
+  async onSubmit(formDirective: FormGroupDirective) {
+    if (this.commentForm.invalid) return;
+
+    const data: Comment = this.commentForm.value;
+    data.momentId = Number(this.moment!.id);
+
+    await this.commentService
+      .createComment(data)
+      .subscribe((comment) => this.moment!.comments!.push(comment.data));
+
+    this.messagesService.add('Comentário adicionado!');
+    this.commentForm.reset();
+
+    formDirective.resetForm();
   }
 }
